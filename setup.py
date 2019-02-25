83d0c53c17c6ee8f30591404e82fb4f65516d4dd
Wang <615349118@163.com>
2019-02-25 22:35:28 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+from setuptools import find_packages, setup
+
+setup(
+    name='rllite',
+    version='0.0.3',
+    author='zjurl',
+    author_email='wangyunkai.zju@gmail.com',
+    url='https://github.com/ZJU-RL/rllite',
+    description=u'rl lite repository',
+    packages=['rllite'],
+    install_requires=[
+    ]
+)
