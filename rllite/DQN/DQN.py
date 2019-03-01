cec4983fb2a7757c478d60e69e128d1926c1ca3a
0aqz0 <18026332256@163.com>
2019-03-01 15:05:34 +0800

    update DQN to new version

--- rllite/DQN/DQN.py
+++ rllite/DQN/DQN.py
@@ -1,121 +1,259 @@
+import math, random
+
+import gym
+import numpy as np
+
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
-import gym
+import torch.optim as optim
 
-# Hyper Parameters
-BATCH_SIZE = 32
-LR = 0.01                   # learning rate
-EPSILON = 0.9               # greedy policy
-GAMMA = 0.9                 # reward discount
-TARGET_REPLACE_ITER = 100   # target update frequency
-MEMORY_CAPACITY = 2000
-env = gym.make('CartPole-v0')
-env = env.unwrapped
-N_ACTIONS = env.action_space.n
-N_STATES = env.observation_space.shape[0]
-ENV_A_SHAPE = 0 if isinstance(env.action_space.sample(), int) else env.action_space.sample().shape     # to confirm the shape
+from IPython.display import clear_output
+import matplotlib.pyplot as plt
 
 
-class Net(nn.Module):
-    def __init__(self, ):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(N_STATES, 50)
-        self.fc1.weight.data.normal_(0, 0.1)   # initialization
-        self.out = nn.Linear(50, N_ACTIONS)
-        self.out.weight.data.normal_(0, 0.1)   # initialization
+USE_CUDA = torch.cuda.is_available()
+
+from collections import deque
+
+
+class ReplayBuffer(object):
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+
+    def push(self, state, action, reward, next_state, done):
+        state = np.expand_dims(state, 0)
+        next_state = np.expand_dims(next_state, 0)
+
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
+        return np.concatenate(state), action, reward, np.concatenate(next_state), done
+
+    def __len__(self):
+        return len(self.buffer)
+
+"""
+Cart Pole Environment
+"""
+env_id = "CartPole-v0"
+env = gym.make(env_id)
+
+epsilon_start = 1.0
+epsilon_final = 0.01
+epsilon_decay = 500
+
+epsilon_by_frame = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx / epsilon_decay)
+
+plt.plot([epsilon_by_frame(i) for i in range(10000)])
+
+
+class DQN(nn.Module):
+    def __init__(self, num_inputs, num_actions):
+        super(DQN, self).__init__()
+
+        self.layers = nn.Sequential(
+            nn.Linear(env.observation_space.shape[0], 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, env.action_space.n)
+        )
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = F.relu(x)
-        actions_value = self.out(x)
-        return actions_value
+        return self.layers(x)
 
-
-class DQN(object):
-    def __init__(self):
-        self.eval_net, self.target_net = Net(), Net()
-
-        self.learn_step_counter = 0                                     # for target updating
-        self.memory_counter = 0                                         # for storing memory
-        self.memory = np.zeros((MEMORY_CAPACITY, N_STATES * 2 + 2))     # initialize memory
-        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=LR)
-        self.loss_func = nn.MSELoss()
-
-    def choose_action(self, x):
-        x = torch.unsqueeze(torch.FloatTensor(x), 0)
-        # input only one sample
-        if np.random.uniform() < EPSILON:   # greedy
-            actions_value = self.eval_net.forward(x)
-            action = torch.max(actions_value, 1)[1].data.numpy()
-            action = action[0] if ENV_A_SHAPE == 0 else action.reshape(ENV_A_SHAPE)  # return the argmax index
-        else:   # random
-            action = np.random.randint(0, N_ACTIONS)
-            action = action if ENV_A_SHAPE == 0 else action.reshape(ENV_A_SHAPE)
+    def act(self, state, epsilon):
+        if random.random() > epsilon:
+            state = torch.FloatTensor(state).unsqueeze(0)
+            q_value = self.forward(state)
+            # print(q_value)
+            print(torch.argmax(q_value, dim=1).item())
+            # action = q_value.max(1)[1].data[0]
+            action = torch.argmax(q_value, dim=1).item()
+        else:
+            action = random.randrange(env.action_space.n)
         return action
 
-    def store_transition(self, s, a, r, s_):
-        transition = np.hstack((s, [a, r], s_))
-        # replace the old memory with new memory
-        index = self.memory_counter % MEMORY_CAPACITY
-        self.memory[index, :] = transition
-        self.memory_counter += 1
 
-    def learn(self):
-        # target parameter update
-        if self.learn_step_counter % TARGET_REPLACE_ITER == 0:
-            self.target_net.load_state_dict(self.eval_net.state_dict())
-        self.learn_step_counter += 1
+model = DQN(env.observation_space.shape[0], env.action_space.n)
 
-        # sample batch transitions
-        sample_index = np.random.choice(MEMORY_CAPACITY, BATCH_SIZE)
-        b_memory = self.memory[sample_index, :]
-        b_s = torch.FloatTensor(b_memory[:, :N_STATES])
-        b_a = torch.LongTensor(b_memory[:, N_STATES:N_STATES+1].astype(int))
-        b_r = torch.FloatTensor(b_memory[:, N_STATES+1:N_STATES+2])
-        b_s_ = torch.FloatTensor(b_memory[:, -N_STATES:])
+if USE_CUDA:
+    model = model.cuda()
 
-        # q_eval w.r.t the action in experience
-        q_eval = self.eval_net(b_s).gather(1, b_a)  # shape (batch, 1)
-        q_next = self.target_net(b_s_).detach()     # detach from graph, don't backpropagate
-        q_target = b_r + GAMMA * q_next.max(1)[0].view(BATCH_SIZE, 1)   # shape (batch, 1)
-        loss = self.loss_func(q_eval, q_target)
+optimizer = optim.Adam(model.parameters())
 
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
+replay_buffer = ReplayBuffer(1000)
 
 
-if __name__ == '__main__':
-    dqn = DQN()
+def compute_td_loss(batch_size):
+    state, action, reward, next_state, done = replay_buffer.sample(batch_size)
 
-    print('\nCollecting experience...')
-    for i_episode in range(400):
-        s = env.reset()
-        ep_r = 0
-        while True:
-            env.render()
-            a = dqn.choose_action(s)
+    state = torch.FloatTensor(np.float32(state))
+    next_state = torch.FloatTensor(np.float32(next_state))
+    action = torch.LongTensor(action)
+    reward = torch.FloatTensor(reward)
+    done = torch.FloatTensor(done)
 
-            # take action
-            s_, r, done, info = env.step(a)
+    q_values = model(state)
+    next_q_values = model(next_state)
 
-            # modify the reward
-            x, x_dot, theta, theta_dot = s_
-            r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
-            r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
-            r = r1 + r2
+    q_value = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
+    next_q_value = next_q_values.max(1)[0]
+    expected_q_value = reward + gamma * next_q_value * (1 - done)
 
-            dqn.store_transition(s, a, r, s_)
+    loss = (q_value - expected_q_value).pow(2).mean()
 
-            ep_r += r
-            if dqn.memory_counter > MEMORY_CAPACITY:
-                dqn.learn()
-                if done:
-                    print('Ep: ', i_episode,
-                          '| Ep_r: ', round(ep_r, 2))
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
 
-            if done:
-                break
-            s = s_
+    return loss
+
+def plot(frame_idx, rewards, losses):
+    clear_output(True)
+    plt.figure(figsize=(20,5))
+    plt.subplot(121)
+    plt.title('frame %s. reward: %s' % (frame_idx, np.mean(rewards[-10:])))
+    plt.plot(rewards)
+    plt.subplot(122)
+    plt.title('loss')
+    plt.plot(losses)
+    plt.show()
+
+
+num_frames = 10000
+batch_size = 32
+gamma = 0.99
+
+losses = []
+all_rewards = []
+episode_reward = 0
+
+state = env.reset()
+for frame_idx in range(1, num_frames + 1):
+    epsilon = epsilon_by_frame(frame_idx)
+    action = model.act(state, epsilon)
+
+    next_state, reward, done, _ = env.step(action)
+    replay_buffer.push(state, action, reward, next_state, done)
+
+    state = next_state
+    episode_reward += reward
+
+    if done:
+        state = env.reset()
+        all_rewards.append(episode_reward)
+        episode_reward = 0
+
+    if len(replay_buffer) > batch_size:
+        loss = compute_td_loss(batch_size)
+        losses.append(loss.item())
+
+    if frame_idx == num_frames:
+        plot(frame_idx, all_rewards, losses)
+
+"""
+Atari Environment
+"""
+from utils import make_atari, wrap_deepmind, wrap_pytorch
+
+env_id = "PongNoFrameskip-v4"
+env = make_atari(env_id)
+env = wrap_deepmind(env)
+env = wrap_pytorch(env)
+
+
+class CnnDQN(nn.Module):
+    def __init__(self, input_shape, num_actions):
+        super(CnnDQN, self).__init__()
+
+        self.input_shape = input_shape
+        self.num_actions = num_actions
+
+        self.features = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(self.feature_size(), 512),
+            nn.ReLU(),
+            nn.Linear(512, self.num_actions)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+    def feature_size(self):
+        return self.features(torch.zeros(1, *self.input_shape)).view(1, -1).size(1)
+
+    def act(self, state, epsilon):
+        if random.random() > epsilon:
+            state = torch.FloatTensor(np.float32(state)).unsqueeze(0)
+            q_value = self.forward(state)
+            # print(q_value)
+            print(torch.argmax(q_value, dim=1).item())
+            # action = q_value.max(1)[1].data[0]
+            action = torch.argmax(q_value, dim=1).item()
+        else:
+            action = random.randrange(env.action_space.n)
+        return action
+
+
+model = CnnDQN(env.observation_space.shape, env.action_space.n)
+
+if USE_CUDA:
+    model = model.cuda()
+
+optimizer = optim.Adam(model.parameters(), lr=0.00001)
+
+replay_initial = 10000
+replay_buffer = ReplayBuffer(100000)
+
+epsilon_start = 1.0
+epsilon_final = 0.01
+epsilon_decay = 30000
+
+epsilon_by_frame = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx / epsilon_decay)
+
+plt.plot([epsilon_by_frame(i) for i in range(1000000)])
+
+num_frames = 1400000
+batch_size = 32
+gamma = 0.99
+
+losses = []
+all_rewards = []
+episode_reward = 0
+
+state = env.reset()
+for frame_idx in range(1, num_frames + 1):
+    epsilon = epsilon_by_frame(frame_idx)
+    action = model.act(state, epsilon)
+
+    next_state, reward, done, _ = env.step(action)
+    replay_buffer.push(state, action, reward, next_state, done)
+
+    state = next_state
+    episode_reward += reward
+
+    if done:
+        state = env.reset()
+        all_rewards.append(episode_reward)
+        episode_reward = 0
+
+    if len(replay_buffer) > replay_initial:
+        loss = compute_td_loss(batch_size)
+        losses.append(loss.item())
+
+    if frame_idx % 10000 == 0:
+        plot(frame_idx, all_rewards, losses)
+
