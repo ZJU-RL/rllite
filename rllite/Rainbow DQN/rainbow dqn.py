c2799ec4e2bee18a486336aa28577695c3d79410
0aqz0 <18026332256@163.com>
2019-03-01 17:08:33 +0800

    add rainbow dqn

---
+++ rllite/Rainbow DQN/rainbow dqn.py
@@ -0,0 +1,330 @@
+import math, random
+
+import gym
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.autograd as autograd
+import torch.nn.functional as F
+
+from utils import NoisyLinear
+from utils import ReplayBuffer
+
+from IPython.display import clear_output
+import matplotlib.pyplot as plt
+
+USE_CUDA = torch.cuda.is_available()
+
+"""
+CartPole
+"""
+env_id = "CartPole-v0"
+env = gym.make(env_id)
+
+
+class RainbowDQN(nn.Module):
+    def __init__(self, num_inputs, num_actions, num_atoms, Vmin, Vmax):
+        super(RainbowDQN, self).__init__()
+
+        self.num_inputs = num_inputs
+        self.num_actions = num_actions
+        self.num_atoms = num_atoms
+        self.Vmin = Vmin
+        self.Vmax = Vmax
+
+        self.linear1 = nn.Linear(num_inputs, 32)
+        self.linear2 = nn.Linear(32, 64)
+
+        self.noisy_value1 = NoisyLinear(64, 64, use_cuda=USE_CUDA)
+        self.noisy_value2 = NoisyLinear(64, self.num_atoms, use_cuda=USE_CUDA)
+
+        self.noisy_advantage1 = NoisyLinear(64, 64, use_cuda=USE_CUDA)
+        self.noisy_advantage2 = NoisyLinear(64, self.num_atoms * self.num_actions, use_cuda=USE_CUDA)
+
+    def forward(self, x):
+        batch_size = x.size(0)
+
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+
+        value = F.relu(self.noisy_value1(x))
+        value = self.noisy_value2(value)
+
+        advantage = F.relu(self.noisy_advantage1(x))
+        advantage = self.noisy_advantage2(advantage)
+
+        value = value.view(batch_size, 1, self.num_atoms)
+        advantage = advantage.view(batch_size, self.num_actions, self.num_atoms)
+
+        x = value + advantage - advantage.mean(1, keepdim=True)
+        x = F.softmax(x.view(-1, self.num_atoms)).view(-1, self.num_actions, self.num_atoms)
+
+        return x
+
+    def reset_noise(self):
+        self.noisy_value1.reset_noise()
+        self.noisy_value2.reset_noise()
+        self.noisy_advantage1.reset_noise()
+        self.noisy_advantage2.reset_noise()
+
+    def act(self, state):
+        state = torch.FloatTensor(state).unsqueeze(0)
+        dist = self.forward(state).data.cpu()
+        dist = dist * torch.linspace(self.Vmin, self.Vmax, self.num_atoms)
+        action = dist.sum(2).max(1)[1].numpy()[0]
+        return action
+
+
+num_atoms = 51
+Vmin = -10
+Vmax = 10
+
+current_model = RainbowDQN(env.observation_space.shape[0], env.action_space.n, num_atoms, Vmin, Vmax)
+target_model = RainbowDQN(env.observation_space.shape[0], env.action_space.n, num_atoms, Vmin, Vmax)
+
+if USE_CUDA:
+    current_model = current_model.cuda()
+    target_model = target_model.cuda()
+
+optimizer = optim.Adam(current_model.parameters(), 0.001)
+
+replay_buffer = ReplayBuffer(10000)
+
+
+def update_target(current_model, target_model):
+    target_model.load_state_dict(current_model.state_dict())
+
+
+update_target(current_model, target_model)
+
+
+def projection_distribution(next_state, rewards, dones):
+    batch_size = next_state.size(0)
+
+    delta_z = float(Vmax - Vmin) / (num_atoms - 1)
+    support = torch.linspace(Vmin, Vmax, num_atoms)
+
+    next_dist = target_model(next_state).data.cpu() * support
+    next_action = next_dist.sum(2).max(1)[1]
+    next_action = next_action.unsqueeze(1).unsqueeze(1).expand(next_dist.size(0), 1, next_dist.size(2))
+    next_dist = next_dist.gather(1, next_action).squeeze(1)
+
+    rewards = rewards.unsqueeze(1).expand_as(next_dist)
+    dones = dones.unsqueeze(1).expand_as(next_dist)
+    support = support.unsqueeze(0).expand_as(next_dist)
+
+    Tz = rewards + (1 - dones) * 0.99 * support
+    Tz = Tz.clamp(min=Vmin, max=Vmax)
+    b = (Tz - Vmin) / delta_z
+    l = b.floor().long()
+    u = b.ceil().long()
+
+    offset = torch.linspace(0, (batch_size - 1) * num_atoms, batch_size).long() \
+        .unsqueeze(1).expand(batch_size, num_atoms)
+
+    proj_dist = torch.zeros(next_dist.size())
+    proj_dist.view(-1).index_add_(0, (l + offset).view(-1), (next_dist * (u.float() - b)).view(-1))
+    proj_dist.view(-1).index_add_(0, (u + offset).view(-1), (next_dist * (b - l.float())).view(-1))
+
+    return proj_dist
+
+
+def compute_td_loss(batch_size):
+    state, action, reward, next_state, done = replay_buffer.sample(batch_size)
+
+    state = torch.FloatTensor(np.float32(state))
+    next_state = torch.FloatTensor(np.float32(next_state))
+    action = torch.LongTensor(action)
+    reward = torch.FloatTensor(reward)
+    done = torch.FloatTensor(np.float32(done))
+
+    proj_dist = projection_distribution(next_state, reward, done)
+
+    dist = current_model(state)
+    action = action.unsqueeze(1).unsqueeze(1).expand(batch_size, 1, num_atoms)
+    dist = dist.gather(1, action).squeeze(1)
+    dist.data.clamp_(0.01, 0.99)
+    loss = -(proj_dist * dist.log()).sum(1)
+    loss = loss.mean()
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    current_model.reset_noise()
+    target_model.reset_noise()
+
+    return loss
+
+def plot(frame_idx, rewards, losses):
+    clear_output(True)
+    plt.figure(figsize=(20,5))
+    plt.subplot(131)
+    plt.title('frame %s. reward: %s' % (frame_idx, np.mean(rewards[-10:])))
+    plt.plot(rewards)
+    plt.subplot(132)
+    plt.title('loss')
+    plt.plot(losses)
+    plt.show()
+
+
+num_frames = 15000
+batch_size = 32
+gamma = 0.99
+
+losses = []
+all_rewards = []
+episode_reward = 0
+
+state = env.reset()
+for frame_idx in range(1, num_frames + 1):
+    action = current_model.act(state)
+
+    next_state, reward, done, _ = env.step(action)
+    replay_buffer.push(state, action, reward, next_state, done)
+
+    state = next_state
+    episode_reward += reward
+
+    if done:
+        state = env.reset()
+        all_rewards.append(episode_reward)
+        episode_reward = 0
+
+    if len(replay_buffer) > batch_size:
+        loss = compute_td_loss(batch_size)
+        losses.append(loss.item())
+
+    # if frame_idx % 200 == 0:
+    if frame_idx == num_frames:
+        plot(frame_idx, all_rewards, losses)
+
+    if frame_idx % 1000 == 0:
+        update_target(current_model, target_model)
+
+"""
+PongNoFrameskip
+"""
+from utils import make_atari, wrap_deepmind, wrap_pytorch
+
+env_id = "PongNoFrameskip-v4"
+env = make_atari(env_id)
+env = wrap_deepmind(env)
+env = wrap_pytorch(env)
+
+
+class RainbowCnnDQN(nn.Module):
+    def __init__(self, input_shape, num_actions, num_atoms, Vmin, Vmax):
+        super(RainbowCnnDQN, self).__init__()
+
+        self.input_shape = input_shape
+        self.num_actions = num_actions
+        self.num_atoms = num_atoms
+        self.Vmin = Vmin
+        self.Vmax = Vmax
+
+        self.features = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.noisy_value1 = NoisyLinear(self.feature_size(), 512, use_cuda=USE_CUDA)
+        self.noisy_value2 = NoisyLinear(512, self.num_atoms, use_cuda=USE_CUDA)
+
+        self.noisy_advantage1 = NoisyLinear(self.feature_size(), 512, use_cuda=USE_CUDA)
+        self.noisy_advantage2 = NoisyLinear(512, self.num_atoms * self.num_actions, use_cuda=USE_CUDA)
+
+    def forward(self, x):
+        batch_size = x.size(0)
+
+        x = x / 255.
+        x = self.features(x)
+        x = x.view(batch_size, -1)
+
+        value = F.relu(self.noisy_value1(x))
+        value = self.noisy_value2(value)
+
+        advantage = F.relu(self.noisy_advantage1(x))
+        advantage = self.noisy_advantage2(advantage)
+
+        value = value.view(batch_size, 1, self.num_atoms)
+        advantage = advantage.view(batch_size, self.num_actions, self.num_atoms)
+
+        x = value + advantage - advantage.mean(1, keepdim=True)
+        x = F.softmax(x.view(-1, self.num_atoms)).view(-1, self.num_actions, self.num_atoms)
+
+        return x
+
+    def reset_noise(self):
+        self.noisy_value1.reset_noise()
+        self.noisy_value2.reset_noise()
+        self.noisy_advantage1.reset_noise()
+        self.noisy_advantage2.reset_noise()
+
+    def feature_size(self):
+        return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
+
+    def act(self, state):
+        state = torch.FloatTensor(np.float32(state)).unsqueeze(0)
+        dist = self.forward(state).data.cpu()
+        dist = dist * torch.linspace(self.Vmin, self.Vmax, self.num_atoms)
+        action = dist.sum(2).max(1)[1].numpy()[0]
+        return action
+
+
+num_atoms = 51
+Vmin = -10
+Vmax = 10
+
+current_model = RainbowCnnDQN(env.observation_space.shape, env.action_space.n, num_atoms, Vmin, Vmax)
+target_model = RainbowCnnDQN(env.observation_space.shape, env.action_space.n, num_atoms, Vmin, Vmax)
+
+if USE_CUDA:
+    current_model = current_model.cuda()
+    target_model = target_model.cuda()
+
+optimizer = optim.Adam(current_model.parameters(), lr=0.0001)
+update_target(current_model, target_model)
+
+replay_initial = 10000
+replay_buffer = ReplayBuffer(100000)
+
+num_frames = 1000000
+batch_size = 32
+gamma = 0.99
+
+losses = []
+all_rewards = []
+episode_reward = 0
+
+state = env.reset()
+for frame_idx in range(1, num_frames + 1):
+    action = current_model.act(state)
+
+    next_state, reward, done, _ = env.step(action)
+    replay_buffer.push(state, action, reward, next_state, done)
+
+    state = next_state
+    episode_reward += reward
+
+    if done:
+        state = env.reset()
+        all_rewards.append(episode_reward)
+        episode_reward = 0
+
+    if len(replay_buffer) > replay_initial:
+        loss = compute_td_loss(batch_size)
+        losses.append(loss.item())
+
+    if frame_idx % 10000 == 0:
+    # if frame_idx == num_frames:
+        plot(frame_idx, all_rewards, losses)
+
+    if frame_idx % 1000 == 0:
+        update_target(current_model, target_model)
