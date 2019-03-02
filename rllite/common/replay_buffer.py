243f7d0dc4d3ba95ae55a0dacec26287af87c1c3
0aqz0 <18026332256@163.com>
2019-03-02 15:01:45 +0800

    update DQN and CnnDQN

--- rllite/common/replay_buffer.py
+++ rllite/common/replay_buffer.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+from collections import deque
 
 class ReplayBuffer:
     def __init__(self, capacity):
@@ -18,5 +19,23 @@ class ReplayBuffer:
         state, action, reward, next_state, done = map(np.stack, zip(*batch))
         return state, action, reward, next_state, done
     
+    def __len__(self):
+        return len(self.buffer)
+
+
+class ReplayBuffer2(object):
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+
+    def push(self, state, action, reward, next_state, done):
+        state = np.expand_dims(state, 0)
+        next_state = np.expand_dims(next_state, 0)
+
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
+        return np.concatenate(state), action, reward, np.concatenate(next_state), done
+
     def __len__(self):
         return len(self.buffer)
