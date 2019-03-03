11180e735dc5b72cee8acbc8a7dbd19ea4d994c7
0aqz0 <18026332256@163.com>
2019-03-03 19:06:17 +0800

    update hierarchical DQN

---
+++ rllite/HierarchicalDQN/__init__.py
@@ -0,0 +1,1 @@
+from .hierarchical_dqn import *
--- rllite/HierarchicalDQN/hierarchical_dqn.py
+++ rllite/HierarchicalDQN/hierarchical_dqn.py
@@ -2,17 +2,14 @@ import math
 import random
 from collections import namedtuple, deque
 
-import gym
 import numpy as np
+import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
-import torch.autograd as autograd
 
-from IPython.display import clear_output
-import matplotlib.pyplot as plt
+from rllite.common import ReplayBuffer2
 
 USE_CUDA = torch.cuda.is_available()
 
@@ -58,27 +55,12 @@ class StochasticMDP:
             return state, 0.0, False, {}
 
 
-class ReplayBuffer(object):
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.buffer = deque(maxlen=capacity)
-
-    def push(self, state, action, reward, next_state, done):
-        state = np.expand_dims(state, 0)
-        next_state = np.expand_dims(next_state, 0)
-        self.buffer.append((state, action, reward, next_state, done))
-
-    def sample(self, batch_size):
-        state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
-        return np.concatenate(state), action, reward, np.concatenate(next_state), done
-
-    def __len__(self):
-        return len(self.buffer)
-
-
 class Net(nn.Module):
     def __init__(self, num_inputs, num_outputs):
         super(Net, self).__init__()
+
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
 
         self.layers = nn.Sequential(
             nn.Linear(num_inputs, 256),
@@ -95,110 +77,111 @@ class Net(nn.Module):
             action = self.forward(state).max(1)[1]
             return action.data[0]
         else:
-            return random.randrange(num_actions)
-
-env = StochasticMDP()
-
-num_goals    = env.num_states
-num_actions  = env.num_actions
-
-model        = Net(2*num_goals, num_actions)
-target_model = Net(2*num_goals, num_actions)
-
-meta_model        = Net(num_goals, num_goals)
-target_meta_model = Net(num_goals, num_goals)
-
-if USE_CUDA:
-    model             = model.cuda()
-    target_model      = target_model.cuda()
-    meta_model        = meta_model.cuda()
-    target_meta_model = target_meta_model.cuda()
-
-optimizer      = optim.Adam(model.parameters())
-meta_optimizer = optim.Adam(meta_model.parameters())
-
-replay_buffer      = ReplayBuffer(10000)
-meta_replay_buffer = ReplayBuffer(10000)
-
-def to_onehot(x):
-    oh = np.zeros(6)
-    oh[x - 1] = 1.
-    return oh
+            return random.randrange(self.num_outputs)
 
 
-def update(model, optimizer, replay_buffer, batch_size):
-    if batch_size > len(replay_buffer):
-        return
-    state, action, reward, next_state, done = replay_buffer.sample(batch_size)
+class HierarchicalDQN(object):
+    def __init__(self):
+        self.env = StochasticMDP()
 
-    state = torch.FloatTensor(state)
-    next_state = torch.FloatTensor(next_state)
-    action = torch.LongTensor(action)
-    reward = torch.FloatTensor(reward)
-    done = torch.FloatTensor(done)
+        self.num_goals = self.env.num_states
+        self.num_actions = self.env.num_actions
 
-    q_value = model(state)
-    q_value = q_value.gather(1, action.unsqueeze(1)).squeeze(1)
+        self.model = Net(2*self.num_goals, self.num_actions)
+        self.target_model = Net(2*self.num_goals, self.num_actions)
 
-    next_q_value = model(next_state).max(1)[0]
-    expected_q_value = reward + 0.99 * next_q_value * (1 - done)
+        self.meta_model = Net(self.num_goals, self.num_goals)
+        self.target_meta_model = Net(self.num_goals, self.num_goals)
 
-    loss = (q_value - expected_q_value).pow(2).mean()
+        if USE_CUDA:
+            self.model = self.model.cuda()
+            self.target_model = self.target_model.cuda()
+            self.meta_model = self.meta_model.cuda()
+            self.target_meta_model = self.target_meta_model.cuda()
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+        self.optimizer = optim.Adam(self.model.parameters())
+        self.meta_optimizer = optim.Adam(self.meta_model.parameters())
 
-epsilon_start = 1.0
-epsilon_final = 0.01
-epsilon_decay = 500
+        self.replay_buffer = ReplayBuffer2(10000)
+        self.meta_replay_buffer = ReplayBuffer2(10000)
 
-epsilon_by_frame = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx / epsilon_decay)
+    def to_onehot(self, x):
+        oh = np.zeros(6)
+        oh[x - 1] = 1.
+        return oh
 
-num_frames = 100000
-frame_idx = 1
+    def update(self, model, optimizer, replay_buffer, batch_size):
+        if batch_size > len(replay_buffer):
+            return
+        state, action, reward, next_state, done = replay_buffer.sample(batch_size)
 
-state = env.reset()
-done = False
-all_rewards = []
-episode_reward = 0
+        state = torch.FloatTensor(state)
+        next_state = torch.FloatTensor(next_state)
+        action = torch.LongTensor(action)
+        reward = torch.FloatTensor(reward)
+        done = torch.FloatTensor(done)
 
-while frame_idx < num_frames:
-    goal = meta_model.act(state, epsilon_by_frame(frame_idx))
-    onehot_goal = to_onehot(goal)
+        q_value = model(state)
+        q_value = q_value.gather(1, action.unsqueeze(1)).squeeze(1)
 
-    meta_state = state
-    extrinsic_reward = 0
+        next_q_value = model(next_state).max(1)[0]
+        expected_q_value = reward + 0.99 * next_q_value * (1 - done)
 
-    while not done and goal != np.argmax(state):
-        goal_state = np.concatenate([state, onehot_goal])
-        action = model.act(goal_state, epsilon_by_frame(frame_idx))
-        next_state, reward, done, _ = env.step(action)
+        loss = (q_value - expected_q_value).pow(2).mean()
 
-        episode_reward += reward
-        extrinsic_reward += reward
-        intrinsic_reward = 1.0 if goal == np.argmax(next_state) else 0.0
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-        replay_buffer.push(goal_state, action, intrinsic_reward, np.concatenate([next_state, onehot_goal]), done)
-        state = next_state
-
-        update(model, optimizer, replay_buffer, 32)
-        update(meta_model, meta_optimizer, meta_replay_buffer, 32)
-        frame_idx += 1
-
-        if frame_idx % 1000 == 0:
-            clear_output(True)
-            n = 100  # mean reward of last 100 episodes
-            plt.figure(figsize=(20, 5))
-            plt.title(frame_idx)
-            plt.plot([np.mean(all_rewards[i:i + n]) for i in range(0, len(all_rewards), n)])
-            plt.show()
-
-    meta_replay_buffer.push(meta_state, goal, extrinsic_reward, state, done)
-
-    if done:
-        state = env.reset()
+    def learn(self, num_frames=100000, epsilon_start=1.0, epsilon_final=0.01, epsilon_decay=500):
+        frame_idx = 1
+        state = self.env.reset()
         done = False
-        all_rewards.append(episode_reward)
+        all_rewards = []
         episode_reward = 0
 
+        while frame_idx < num_frames:
+            goal = self.meta_model.act(state, epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx / epsilon_decay))
+            onehot_goal = self.to_onehot(goal)
+
+            meta_state = state
+            extrinsic_reward = 0
+
+            while not done and goal != np.argmax(state):
+                goal_state = np.concatenate([state, onehot_goal])
+                action = self.model.act(goal_state, epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx / epsilon_decay))
+                next_state, reward, done, _ = self.env.step(action)
+
+                episode_reward += reward
+                extrinsic_reward += reward
+                intrinsic_reward = 1.0 if goal == np.argmax(next_state) else 0.0
+
+                self.replay_buffer.push(goal_state, action, intrinsic_reward, np.concatenate([next_state, onehot_goal]),
+                                   done)
+                state = next_state
+
+                self.update(self.model, self.optimizer, self.replay_buffer, 32)
+                self.update(self.meta_model, self.meta_optimizer, self.meta_replay_buffer, 32)
+                frame_idx += 1
+
+                if frame_idx % 1000 == 0:
+                    n = 100  # mean reward of last 100 episodes
+                    plt.figure(figsize=(20, 5))
+                    plt.title(frame_idx)
+                    plt.plot([np.mean(all_rewards[i:i + n]) for i in range(0, len(all_rewards), n)])
+                    plt.show()
+
+            self.meta_replay_buffer.push(meta_state, goal, extrinsic_reward, state, done)
+
+            if done:
+                state = self.env.reset()
+                done = False
+                all_rewards.append(episode_reward)
+                episode_reward = 0
+
+            print(frame_idx)
+
+
+if __name__ == '__main__':
+    model = HierarchicalDQN()
+    model.learn()
