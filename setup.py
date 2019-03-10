ef1490d0e0caa9d8fe6674fdf03d078ac25f8f1c
Wang <615349118@163.com>
2019-03-10 15:37:04 +0800

    add dSAC

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='rllite',
-    version='0.1.1',
+    version='0.1.2',
     author='zjurl',
     author_email='wangyunkai.zju@gmail.com',
     url='https://github.com/ZJU-RL/rllite',
